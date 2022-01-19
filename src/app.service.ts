ed0a91f9302dc7ee5bfe94a0d3097b2c4b61cc57
Ilingu <thetitouoff@gmail.com>
2022-01-19 23:02:59 +0100

    Test

--- src/app.service.ts
+++ src/app.service.ts
@@ -21,10 +21,34 @@ export class AppService {
     }
     this.logger.warn('Old Cached Data, recreate new Data');
 
-    const NewDom = this.queryNewData();
-    if (!NewDom) return null;
+    this.queryTest();
+    return null;
+    // const NewDom = this.queryNewData();
+    // if (!NewDom) return null;
 
-    return NewDom as unknown as AdkamiNewEpisodeShape[];
+    // return NewDom as unknown as AdkamiNewEpisodeShape[];
+  }
+
+  async queryTest() {
+    try {
+      this.axios.get('https://ack.vercel.app/', {
+        headers: {
+          accept:
+            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+          'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+          'cache-control': 'max-age=0',
+          'sec-fetch-dest': 'document',
+          'sec-fetch-mode': 'navigate',
+          'sec-fetch-site': 'none',
+          'sec-fetch-user': '?1',
+          'sec-gpc': '1',
+          'upgrade-insecure-requests': '1',
+          'referrer-policy': 'strict-origin-when-cross-origin',
+        },
+      });
+    } catch (err) {
+      this.logger.error(`Failed QueryTest`, err.stack);
+    }
   }
 
   async queryNewData(): Promise<AdkamiNewEpisodeShape[] | false> {
