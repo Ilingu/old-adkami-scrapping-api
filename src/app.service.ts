23b6e747bdee6d209566c30cffc89a2d83ee34d9
Ilingu <thetitouoff@gmail.com>
2022-01-19 23:05:30 +0100

    Test 2

--- src/app.service.ts
+++ src/app.service.ts
@@ -31,21 +31,24 @@ export class AppService {
 
   async queryTest() {
     try {
-      this.axios.get('https://ack.vercel.app/', {
-        headers: {
-          accept:
-            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-          'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
-          'cache-control': 'max-age=0',
-          'sec-fetch-dest': 'document',
-          'sec-fetch-mode': 'navigate',
-          'sec-fetch-site': 'none',
-          'sec-fetch-user': '?1',
-          'sec-gpc': '1',
-          'upgrade-insecure-requests': '1',
-          'referrer-policy': 'strict-origin-when-cross-origin',
-        },
-      });
+      const data = await firstValueFrom(
+        this.axios.get('https://ack.vercel.app/', {
+          headers: {
+            accept:
+              'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+            'cache-control': 'max-age=0',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'sec-gpc': '1',
+            'upgrade-insecure-requests': '1',
+            'referrer-policy': 'strict-origin-when-cross-origin',
+          },
+        }),
+      );
+      console.log(data.data);
     } catch (err) {
       this.logger.error(`Failed QueryTest`, err.stack);
     }
