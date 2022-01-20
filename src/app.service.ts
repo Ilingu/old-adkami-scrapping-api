45516745900768e202dcecbdf9969aff234746ff
Ilingu <thetitouoff@gmail.com>
2022-01-20 09:20:28 +0100

    Test Bypass CloudFare

--- src/app.service.ts
+++ src/app.service.ts
@@ -32,24 +32,12 @@ export class AppService {
       const HtmlDocRes = await firstValueFrom(
         this.axios.get('https://www.adkami.com/', {
           headers: {
-            accept:
-              'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
-            'cache-control': 'max-age=0',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'sec-gpc': '1',
-            'upgrade-insecure-requests': '1',
-            'referrer-policy': 'strict-origin-when-cross-origin',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods':
-              'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-            referrer: 'https://www.adkami.com/',
+            'User-Agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
           },
         }),
       );
+      console.log(HtmlDocRes.headers);
       const DOM = new JSDOM(HtmlDocRes.data);
       const ReleasedAnimeDiv =
         DOM.window.document.querySelectorAll('.video-item-list') || null;
